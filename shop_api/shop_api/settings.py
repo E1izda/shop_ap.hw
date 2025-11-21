dd0c0787b4b1a76e095a3ef1a68aac7d58cfd51a
E1izda <elizdademo@gmail.com>
2025-11-21 17:50:58 +0600

    hw7

--- shop_api/shop_api/settings.py
+++ shop_api/shop_api/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'shop_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop_db',
+        'USER': 'shop_api_user',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- shop_api/users/models.py
+++ shop_api/users/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class ConfirmationCode(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
